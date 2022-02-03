ead4ed10b77b2b1e8800a881984b5cb752918125
mdyousufhossain <yousafhossain@gmail.com>
2022-02-03 23:04:35 +0600

    navbardone

---
+++ public/app.js
@@ -0,0 +1,17 @@
+const body = document.querySelector('body'),
+      nav  = document.querySelector('nav'),
+      modeToggle = document.querySelector(".dark-light"),
+      searchToggle = document.querySelector(".searchBox")
+
+
+    //   ===Event Listener===
+
+    // === Dark Mode Toggler ==
+    modeToggle.addEventListener("click",() =>{
+        modeToggle.classList.toggle('active')
+        body.classList.toggle('dark');
+
+    })
+
+
+
