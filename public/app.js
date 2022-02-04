e372dca895921efe1a48ded7f0364d56daa3a2a0
mdyousufhossain <43577099+mdyousufhossain@users.noreply.github.com>
2022-02-04 13:57:48 +0600

    major changes in javascript

--- public/app.js
+++ public/app.js
@@ -2,7 +2,9 @@
 const body = document.querySelector('body'),
       nav  = document.querySelector('nav'),
       modeToggle = document.querySelector(".dark-light"),
-      searchToggle = document.querySelector(".searchBox")
+      searchToggle = document.querySelector(".searchBox");
+
+      
 
 
     //   ===Event Listener===
@@ -12,7 +14,22 @@ const body = document.querySelector('body'),
         modeToggle.classList.toggle('active')
         body.classList.toggle('dark');
 
-    })
+        if(!body.classList.contains("dark")){
+            localStorage.setItem("mode","light-mode");
+        }else{
+            localStorage.setItem("mode","dark-mode");
 
+        }
+
+    })
+//  ===Save in LocalStorage ===
+
+let getMode = localStorage.getItem("mode");
+    if(getMode && getMode === "dark-mode"){
+        body.classList.toggle("dark");
+    }
+    
+
+    
 
 
